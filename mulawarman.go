01a3cf36aab4a674b0101dd7485b91a7d00c5d92
Ramadani <email.ramadani@gmail.com>
2021-08-28 11:48:20 +0800

    setup update balance contract

--- mulawarman.go
+++ mulawarman.go
@@ -1,1 +1,24 @@
 package mulawarman
+
+import (
+	"context"
+	"time"
+)
+
+// UpdateBalanceRequest data request to update balance
+type UpdateBalanceRequest struct {
+	ID     string
+	Amount float64
+}
+
+// UpdateBalanceResult return updated balance and time
+type UpdateBalanceResult struct {
+	Amount    float64
+	Balance   float64
+	UpdatedAt time.Time
+}
+
+// UpdateBalance command interface to update balance
+type UpdateBalance interface {
+	Do(ctx context.Context, req *UpdateBalanceRequest) (*UpdateBalanceResult, error)
+}
